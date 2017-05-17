edb04eb60fb64a6ce6c380542e79af2e19b84aea
bgallagher-9 <b.gallagher9@gmail.com>
2017-05-17 11:52:23 -0500

    added recipe puppy search with express

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,25 +6,123 @@ import $ from 'jquery';
 
 
 class App extends Component {
-  componentDidMount() {
+  constructor(){
+    super();
+    this.state = {
+      queryInputValue: '',
+      liveInputValue: '',
+      recipes: [],
+      filters: [],
+      filterInputValues: ''
+    }
+  }
+
+
+
+  queryForRecipes() {
     $.ajax({
-      url: '/api/recipes'
+      url: `/api/recipes?foodQuery={this.state.liveQueryValue}&ingredientQuery={this.state.filters.join()}`
     })
     .done((data) => {
       console.log('data!', data);
+
+      const fixedData = data.results.map((recipe) => {
+        let thumbnail = recipe.thumbnail !== '' ? recipe.thumbnail: 'no-image.png'
+        return {
+          href: recipe.href,
+          thumbnail: thumbnail,
+          ingredients: recipe.ingredients,
+          title: recipe.title
+        }
+      })
+      this.setState({
+        recipe: fixedData
+      });
     });
   }
 
+  handleChange(evt) {
+    this.setState({
+      queryInputValue: evt.target.value
+    })
+  }
+
+  handleKeyUp(evt) {
+    if (evt.keyCode === 13) {
+      this.setState({
+        liveQueryValue: evt.target.value,
+        queryInputValue: ''
+      }, () => {
+        this.queryForRecipes();
+      });
+    }
+  }
+
+  handleSearchClick() {
+    this.setState({
+      liveQueryValue: this.state.queryInputValue,
+      queryInputValue: ''
+    }, () => {
+      this.queryForRecipes();
+    });
+  }
+
+  handleFilterOnKeyUp(evt) {
+    if (evt.keyCode === 13) {
+      const filterArray = this.state.filters.slice();
+      filterArray.push(this.state.filterInputValues);
+    }
+    this.setState({
+      filters: filterArray,
+      filterInputValues: ''
+    }, () => {
+      this.queryForRecipes();
+    });
+  }
+
+  handleFilterInputChange(evt) {
+    this.setState({
+      filterInputValues: evt.target.value
+    })
+  }
+
   render() {
+    const items = this.state.recipes.map((recipes, 1) => {
+      return <li key={recipes + i}>
+        <img src={recipes.thumbnail} alt={recipes.title} />
+        {recipe.title}
+      </li>
+    });
+
+    let filter;
+    if (this.state.recipes.length > 0) {
+      const filters = this.state.filters.map((filter, i) => {
+        return <li key={filter + i}>{filter}</li>
+      });
+      filter = <div>
+        <input
+          onChange={(evt) => this.handleFilterInputChange(evt)}
+          onKeyUp={(evt) => this.handleFilterOnKeyUp(evt)}
+          value={this.state.filterInputValues}
+          />
+        <ol>
+          {filters}
+        </ol>
+      </div>
+    }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="recipe-puppy-searc">
+        <input
+          onChange={(evt) => this.handleChange(evt)}
+          onKeyUp={(evt) => this.handleKeyUp(evt)}
+          value={this.state.queryInputValue}
+          />
+        <button onClick={() => this.handleSearchClick()}>SEARCH</button>
+      {filter}
+      <ol>
+        {items}
+      </ol>
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-});
+// app.get('/api/recipes', function (req, res) {
+//   res.set('Content-Type', 'application/json');
+//   res.send('{"message":"Hello from the custom server!"}');
+// });
 
 app.get('/api/recipes', function(req, res) {
-  request('http://www.recipepuppy.com/api/?q=steak&i=sour%20cream', function(err, resp, body) {
+  request(`http://www.recipepuppy.com/api/?q=${req.body.foodQuery}i=${req.body.ingredientQuery}`, function(err, resp, body) {
     body = JSON.parse(body);
     res.send(body)
   });
